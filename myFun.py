5f9dedd99968df6a77521bdc7dc400a428e50cc6
XDerHpl <135301226+XDerHpl@users.noreply.github.com>
2023-07-30 11:47:38 +0800

    Update myFun.py

--- myFun.py
+++ myFun.py
@@ -1,7 +1,8 @@
 import cv2
 import torch.nn as nn
 import torch.norm as norm
-def myLoss():
+import torch
+def myLoss(y_hat, y, img, origin_img):
   loss1 = nn.CrossEntropyLoss()
-  loss2 = norm
+  loss2 = norm(img - origin_img, p = inf, dim = None, keepdim = True, dtype = torch.float)
        
